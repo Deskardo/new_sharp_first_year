eb14dab96385a88c6b016085f258aa3745bacf04
Deskardo <kalugin.aleksei.v@gmail.com>
2024-02-17 17:09:23 +0300

    type_float_to_divide

--- home_work/home_work/Program.cs
+++ home_work/home_work/Program.cs
@@ -8,31 +8,40 @@
             { 
                 if (int.TryParse(args[0], out int a) && int.TryParse(args[2], out int b)) 
                 { 
-                    switch (args[2]){
+                    switch (args[1]){
                         case "+":
                             Console.WriteLine($"Сумма {a} и {b} = {a + b}");
                             break;
                         case "-":
                             Console.WriteLine($"Разность {a} и {b} = {a - b}");
                             break;
-                        case "":
-                            Console.WriteLine($"Разность {a} и {b} = {a - b}");
+                        case "/":
+                            if (b != 0)
+                            {
+                                float c = (float)a / (float)b;
+                                Console.WriteLine($"Частное от деления {a} на {b} = {c}");
+                            }
+                            else Console.WriteLine("На ноль делить нельзя");
                             break;
-                        case "-":
-                            Console.WriteLine($"Разность {a} и {b} = {a - b}");
+                        case "*":
+                            Console.WriteLine($"Произведение {a} и {b} = {a * b}");
                             break;
                         default: 
-                            Console.WriteLine(); 
+                            Console.WriteLine("Введены некореектные данные, выражение должно быть в формате a operator b, где operator +, -, /, *"); 
                             break;
                     }
 
+                }
+                else
+                {
+                    Console.WriteLine("Введены некореектные данные, выражение должно быть в формате a operator b, где operator +, -, /, *");
                 }
             }
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calc(args);
         }
     }
 }
